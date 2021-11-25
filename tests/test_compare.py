ef4b20db92567ef178d575008fadc721a38d37f1
sahananagaraj9 <74949041+sahananagaraj9@users.noreply.github.com>
2021-11-25 12:01:07 +0530

    Update test_compare.py

--- tests/test_compare.py
+++ tests/test_compare.py
@@ -47,7 +47,7 @@ def test_hub_compare_Channel2():
         granularity="bulk",
         tables=["customer"]
     )
-    assert result["job"] == "chanat-cmp-sourceat-targetat"
+    assert result["job"] == "chanrl-cmp-sourceat-targetat"
     assert result["posted_ev_id"]
 
 
